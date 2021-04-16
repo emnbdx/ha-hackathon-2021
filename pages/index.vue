294da9d0958465695166d5be203b9cf7b3703265
RilDev <c.public@tutanota.com>
2021-04-16 14:17:05 +0200

    clean up design before update to new design

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,56 @@
 <template>
-
+  <div class="">
+    <h1 class="text-4xl text-center mb-6">Bienvenue sur IncubAsso !</h1>
+    <ul class="space-y-2 text-lg mb-6">
+      <li>
+        <label class="flex items-center">
+          <input
+            v-model="isAtLeastTwo"
+            type="checkbox"
+            class="form-checkbox h-6 w-6 mr-2"
+          />
+          Vous êtes au moins 2 ?
+        </label>
+      </li>
+      <li>
+        <label class="flex items-center">
+          <input
+            v-model="isProjectDefined"
+            type="checkbox"
+            class="form-checkbox h-6 w-6 mr-2"
+          />
+          Vous avez un projet stylé ?
+        </label>
+      </li>
+      <li>
+        <label class="flex items-center">
+          <input
+            v-model="isWhishesToChangeTheWorld"
+            type="checkbox"
+            class="form-checkbox h-6 w-6 mr-2"
+          />
+          Vous avez envie de changer votre monde ?
+        </label>
+      </li>
+    </ul>
+    <nuxt-link
+      to="/"
+      class="bg-blue-500 hover:bg-blue-600 transition p-5 px-10 rounded-md text-white text-lg disabled:bg-gray-500 block text-center"
+      disabled="true"
+    >
+      Hé bah c'est parti !
+    </nuxt-link>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isAtLeastTwo: false,
+      isProjectDefined: false,
+      isWhishesToChangeTheWorld: false,
+    }
+  },
+}
+</script>
